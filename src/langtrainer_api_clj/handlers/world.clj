9549f57c46c7aa61cbff03db9d6ef4e7df9595fe
Yury Kotov <bairkan@yandex.ru>
2015-08-17 14:30:50 +0300

    World handler: result processing

--- src/langtrainer_api_clj/handlers/world.clj
+++ src/langtrainer_api_clj/handlers/world.clj
@@ -5,13 +5,21 @@
             [langtrainer-api-clj.models.course :as course]
             [langtrainer-api-clj.models.user :as user]))
 
+(defn transform-unit [course-row unit-row]
+  (-> unit-row
+    (assoc :course_slug (:slug course-row))
+    (assoc :current_step {})))
+
+(defn transform-course [row]
+  (assoc row :units (mapv (partial transform-unit row) (:units row))))
+
 (defn show-fn [data]
   (fn [token]
     (let [models (:models data)
           current-user (user/fetch (:user models) token)]
         (response {:token (:token current-user)
                    :languages (mapv #(select-keys % [:slug]) (language/published))
-                   :courses (course/for-world models)}))))
+                   :courses (mapv transform-course (course/for-world models))}))))
 
 (defn new-world-routes [data]
   (routes
